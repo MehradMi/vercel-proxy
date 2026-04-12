6bd237cb532cab9b7463e5b238ca71bcb9316567
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-04-12 19:22:00 +0330

    pls work

---
+++ api/proxy.js
@@ -0,0 +1,18 @@
+const { createServer } = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({
+  target: 'http://216.144.228.182:10000',
+  ws: true,
+  changeOrigin: true
+});
+
+module.exports = (req, res) => {
+  proxy.web(req, res);
+};
+
+module.exports.config = {
+  api: {
+    bodyParser: false
+  }
+};
