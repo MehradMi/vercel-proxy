2dfdf84d3cb50247bde129da5f87f83dafd71446
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-04-13 12:28:30 +0330

    will it work this time?

--- api/proxy.js
+++ api/proxy.js
@@ -1,43 +1,21 @@
-export const config = {
-  runtime: 'edge',
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({
+  target: 'http://216.144.228.182.nip.io:10000',
+  changeOrigin: true,
+  timeout: 8000,
+});
+
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  proxy.web(req, res, (err) => {
+    res.statusCode = 502;
+    res.end(JSON.stringify({ error: err.message }));
+  });
 };
 
-export default async function handler(req) {
-  try {
-    const body = req.body ? await req.arrayBuffer() : null;
-    
-    const url = new URL(req.url);
-    const targetUrl = new URL('http://216.144.228.182:10000');
-    targetUrl.pathname = url.pathname;
-    targetUrl.search = url.search;
-
-    const headers = new Headers();
-    for (const [key, value] of req.headers) {
-      if (!['host', 'connection'].includes(key.toLowerCase())) {
-        headers.set(key, value);
-      }
-    }
-
-    const response = await fetch(targetUrl.toString(), {
-      method: req.method,
-      headers,
-      body: body,
-    });
-
-    const responseHeaders = new Headers();
-    for (const [key, value] of response.headers) {
-      responseHeaders.set(key, value);
-    }
-    responseHeaders.set('Access-Control-Allow-Origin', '*');
-
-    return new Response(response.body, {
-      status: response.status,
-      headers: responseHeaders,
-    });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), {
-      status: 502,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
-}
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+};
